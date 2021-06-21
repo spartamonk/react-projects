4411436dac2c9e336a4162911b4c6d8c76cf7517
Christopher Akoto <71773056+spartamonk@users.noreply.github.com>
2021-06-21 02:53:14 -0500

    update

--- 17-random-person/setup/src/App.js
+++ 17-random-person/setup/src/App.js
@@ -10,7 +10,82 @@ import {
 const url = 'https://randomuser.me/api/'
 const defaultImage = 'https://randomuser.me/api/portraits/men/75.jpg'
 function App() {
-  return <h2>random user starter</h2>
+  const [isLoading, setIsLoading] = useState(true)
+  const [randomUser, setRandomUser] = useState(null)
+  const [value, setValue] = useState('random person')
+  const [title, setTitle] = useState('name')
+
+  const getRandomPerson = async () => {
+    setIsLoading(true)
+    const response = await fetch(url)
+    const data = await response.json()
+    const randomUser = data.results[0]
+    const {
+      name: { first, last },
+      location: {
+        street: { number, name },
+      },
+      email,
+      login: { password },
+      dob: { age },
+      phone,
+      picture: { large },
+    } = randomUser
+    const newUser = {
+      name: `${first} ${last}`,
+      address: `${number} ${name}`,
+      email,
+      password,
+      age,
+      phone,
+      image: large,
+    }
+    setRandomUser(newUser);
+    setIsLoading(true);
+    setValue(name);
+    setTitle('name');
+  }
+
+  useEffect(() => {
+    getRandomPerson();
+  }, [])
+
+  return (
+    <main>
+      <div className='block bcg-black'></div>
+      <div className='block'>
+        <div className='container'>
+          <img
+            src={(randomUser.image && randomUser.image) || defaultImage}
+            alt={randomUser.name}
+            className='user-img'
+          />
+          <p className='user-title'>{`My ${title} is`}</p>
+          <p className='user-value'>{randomUser.value}</p>
+          <div className='values-list'>
+            <button className='icon' data-label='name'>
+              <FaUser />
+            </button>
+            <button className='icon' data-label='email'>
+              <FaEnvelopeOpen />
+            </button>
+            <button className='icon' data-label='age'>
+              <FaCalendarTimes />
+            </button>
+            <button className='icon' data-label='address'>
+              <FaMap />
+            </button>
+            <button className='icon' data-label='phone'>
+              <FaPhone />
+            </button>
+            <button className='icon' data-label='password'>
+              <FaLock />
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
 }
 
 export default App
